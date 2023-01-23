f5874aa6bda29b5591a3d8581c11c9a3577815f7
Rt-Bitbucket <210020@jc-21.jp>
2023-01-23 21:56:43 +0900

    EnemyManagerの作成途中

--- Enemy/EnemyManager.cpp
+++ Enemy/EnemyManager.cpp
@@ -1,1 +1,74 @@
 #include "EnemyManager.h"
+#include "../Wall.h"
+
+EnemyManager::EnemyManager(GameObject* parent)
+{
+}
+
+EnemyManager::~EnemyManager()
+{
+}
+
+void EnemyManager::Initialize()
+{
+}
+
+void EnemyManager::Update()
+{
+}
+
+void EnemyManager::Draw()
+{
+}
+
+void EnemyManager::Release()
+{
+}
+
+bool EnemyManager::HaveWall(Transform* trans)
+{
+	Wall* pWall = (Wall*)FindObject("Wall");
+
+	int checkX, checkZ;
+
+	// ‰E•ûŒü
+	{
+		checkX = (int)trans->position_.x + 0.3f;
+		checkZ = (int)trans->position_.z;
+		if (pWall->IsWall(checkX, checkZ))
+		{
+			return true;
+		}
+	}
+	// ¶•ûŒü
+	{
+		checkX = (int)trans->position_.x - 0.3f;
+		checkZ = (int)trans->position_.z;
+		if (pWall->IsWall(checkX, checkZ))
+		{
+			return true;
+		}
+	}
+	// ã•ûŒü
+	{
+		checkX = (int)trans->position_.x;
+		checkZ = (int)trans->position_.z + 0.3f;
+		if (pWall->IsWall(checkX, checkZ))
+		{
+			return true;
+		}
+	}
+	// ‰º•ûŒü
+	{
+		checkX = (int)trans->position_.x;
+		checkZ = (int)trans->position_.z - 0.3f;
+		if (pWall->IsWall(checkX, checkZ))
+		{
+			return true;
+		}
+	}
+
+
+
+	return false;
+}
--- Enemy/EnemyManager.h
+++ Enemy/EnemyManager.h
@@ -1,5 +1,34 @@
 #pragma once
-class EnemyManager
+#include "../Engine/GameObject.h"
+
+class EnemyManager : public GameObject
 {
+	enum EnemyType
+	{
+		Red = 0,
+		Green,
+		Pink,
+		Orange,
+		MAX_TYPE
+	};
+
+	EnemyType EnemyHandle;
+
+
+public:
+	EnemyManager(GameObject* parent);
+	~EnemyManager();
+	void Initialize() override;
+	void Update()override;
+	void Draw() override;
+	void Release()override;
+
+	
+	/// <summary>
+	/// •Ç‚È‚çtrue
+	/// </summary>
+	/// <param name="trans"></param>
+	/// <returns></returns>
+	bool HaveWall(Transform* trans);
 };
 
